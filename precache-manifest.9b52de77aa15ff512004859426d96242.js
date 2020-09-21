246eac86b4a35517b11aae8b1ce414e5d040db5c
Kishan Takoordyal <kt@esokia-webagency.com>
2020-09-21 13:49:54 +0400

    update

---
+++ precache-manifest.9b52de77aa15ff512004859426d96242.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d7021be24f7ff14ac6881c79da0417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef350e39e25d847a7405",
+    "url": "/static/js/2.0e862441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0e862441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3c102d8a776f2f2274",
+    "url": "/static/js/main.eb6ebb89.chunk.js"
+  },
+  {
+    "revision": "23ba0f05b3692de47532",
+    "url": "/static/js/runtime-main.4953e6cd.js"
+  },
+  {
+    "revision": "c660281905a931dabd73f0ff770a2048",
+    "url": "/static/media/2000px-Japan_location_map.svg.c6602819.jpg"
+  },
+  {
+    "revision": "9d269d98fd264e9013a43a7146152398",
+    "url": "/static/media/DSC02104.9d269d98.jpeg"
+  },
+  {
+    "revision": "a84cc2e2a0f8a9cda9d9e1872a23e2a2",
+    "url": "/static/media/DSC02548.a84cc2e2.jpg"
+  },
+  {
+    "revision": "385e0e9d4da77c7cd8a24e8c04ccc8dc",
+    "url": "/static/media/Manji_sai.385e0e9d.jpg"
+  },
+  {
+    "revision": "4a02c89cac65058375a59bfb7e5d58c7",
+    "url": "/static/media/Muge_cheval_ryu_kyu.4a02c89c.jpg"
+  },
+  {
+    "revision": "4235ef76cf39174ab46b109851786f35",
+    "url": "/static/media/OKINAWA map.4235ef76.jpg"
+  },
+  {
+    "revision": "0d88390274a4e182c5a7d3176844134c",
+    "url": "/static/media/banner.0d883902.jpg"
+  },
+  {
+    "revision": "e4e7e244216c4463ea69606c9d33105a",
+    "url": "/static/media/bo.e4e7e244.jpg"
+  },
+  {
+    "revision": "1061c24670249d8431555c35695de902",
+    "url": "/static/media/dojoprof.1061c246.jpg"
+  },
+  {
+    "revision": "4782b9072dbac171182d3553ccba513a",
+    "url": "/static/media/dsc00498.4782b907.jpg"
+  },
+  {
+    "revision": "4c2fa27284c7f956dd4e9da654fd5ea5",
+    "url": "/static/media/eku.4c2fa272.jpg"
+  },
+  {
+    "revision": "8d62c0b6ed909035d473aa11b058ad3f",
+    "url": "/static/media/kama.8d62c0b6.jpg"
+  },
+  {
+    "revision": "b3d46e2a47122f8886bd88a1deac1999",
+    "url": "/static/media/location.b3d46e2a.png"
+  },
+  {
+    "revision": "d7e76f3f72df2d859b8958a0d4dcbe00",
+    "url": "/static/media/muge.d7e76f3f.jpg"
+  },
+  {
+    "revision": "e66f2abf27d852f85de48be0c87fdf89",
+    "url": "/static/media/nunti.e66f2abf.jpg"
+  },
+  {
+    "revision": "6cba11149e695cc447de78611266ed5e",
+    "url": "/static/media/sai.6cba1114.jpg"
+  },
+  {
+    "revision": "4e5f0caeecb3caa83a5ae4945d17b8e2",
+    "url": "/static/media/sansetsukon.4e5f0cae.jpg"
+  },
+  {
+    "revision": "f87ec0080ef60e0eed62a505430ba333",
+    "url": "/static/media/sensei_Adaniya_NAMT.f87ec008.jpg"
+  },
+  {
+    "revision": "461705ca3d182c8020f63d781d5596fd",
+    "url": "/static/media/suruchin.461705ca.jpg"
+  },
+  {
+    "revision": "3c8ddd526e3c0dce0935a4e2b072491e",
+    "url": "/static/media/tekko.3c8ddd52.jpg"
+  },
+  {
+    "revision": "87f0b7fcc311ccbb6a6f0a8cc2ea31ad",
+    "url": "/static/media/tunfa.87f0b7fc.jpg"
+  }
+]);
